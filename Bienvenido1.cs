1836e6376fb4e249ea2c5ee5436b25e3aa6a01ed
Emmanuel94 <emagigena94@hotmail.com>
2022-09-30 22:04:25 -0300

    Agregar archivos de proyecto.

---
+++ Bienvenido1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+public class Comparacion
+{
+    public static void Main(string[] args)
+    {
+        int numero1;
+        int numero2;
+
+
+        Console.Write("Escriba el primer entero:");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Escriba el segundo entero:");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+
+
+        if ( numero1 == numero2)
+            Console.WriteLine("{0  == ¨{1", numero1, numero1 2 );
+
+        if ( numero1  != numero2 )
+                Console.WriteLine("{0 != {1", numero1, numero2 );
+
+        if ( numero1 < numero2)
+            Console.WriteLine("{0 < {1", numero1, numero2 );
+
+        if ( numero1 > numero2)
+            Console.WriteLine("{0 <= {1", numero1, numero2 );
+
+        if ( numero1 >= numero2)
+            Console.WriteLine("{0 >= {1", numero1, numero2 );
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
